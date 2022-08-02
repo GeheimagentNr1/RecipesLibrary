e203750fbc2d504282c7f7b4cfc9805588f81042
GeheimagentNr1 <geheimelache@hotmail.com>
2022-08-02 20:36:19 +0200

    Ported to 1.19.1

--- src/main/java/de/geheimagentnr1/recipes_lib/handlers/ModEventHandler.java
+++ src/main/java/de/geheimagentnr1/recipes_lib/handlers/ModEventHandler.java
@@ -1,12 +1,10 @@
 package de.geheimagentnr1.recipes_lib.handlers;
 
 import de.geheimagentnr1.recipes_lib.RecipesLibrary;
-import de.geheimagentnr1.recipes_lib.elements.RegistryEntry;
 import de.geheimagentnr1.recipes_lib.elements.recipes.RecipeSerializers;
 import de.geheimagentnr1.recipes_lib.elements.recipes.ingredients.IngredientSerializer;
 import de.geheimagentnr1.recipes_lib.elements.recipes.ingredients.IngredientSerializers;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
